681436f669b4c58fce8a4f298be8720b3278827d
Nolan Conaway <nolanbconaway@gmail.com>
2020-06-07 13:40:27 -0400

    update sqlite schema

--- build/make_sqlite.py
+++ build/make_sqlite.py
@@ -28,36 +28,37 @@ def lines_in_file(filepath: Path) -> int:
 
 
 def make_fresh_db(db_path: Path) -> None:
-    """Clear out the tables needed for the database, then rebuild."""
+    """Execute the DDL needed for a fresh database."""
+    ddl = """
+    create table artists (
+        artist_id integer primary key,
+        name text not null
+    );
+
+    create table groups (
+        group_id integer primary key,
+        artist_id integer not null,
+        name text not null,
+        snatch_count integer not null default 1,
+        foreign key (artist_id) references artists(artist_id)
+    );
+
+    create table users (
+        user_id integer primary key, 
+        snatch_count integer not null
+    );
+
+    create table snatches (
+        snatch_id integer primary key,
+        user_id integer not null,
+        group_id integer not null,
+        foreign key (group_id) references groups(group_id),
+        foreign key (user_id) references users(user_id),
+        unique(user_id, group_id)
+    );
+    """
     with sqlite3.connect(db_path) as conn:
-        conn.execute("drop table if exists snatches")
-        conn.execute("drop table if exists groups")
-        sql = """
-        create table groups (
-            group_id integer primary key,
-            name text not null,
-            artist_name text not null
-        )
-        """
-        conn.execute(sql)
-        sql = """
-        create table snatches (
-            snatch_id integer primary key,
-            user_id integer not null,
-            group_id integer not null,
-            foreign key (group_id) references groups(group_id),
-            unique(user_id, group_id)
-        )
-        """
-        conn.execute(sql)
-        sql = """
-        create view support as 
-        select group_id, count(*) as freq, count(*) / (n + 0.0) as p
-        from snatches
-        cross join (select count(*) as n from snatches) as t
-        group by 1
-        """
-        conn.execute(sql)
+        conn.executescript(ddl)
 
 
 def process_line(db_path: Path, line: str) -> None:
@@ -66,6 +67,7 @@ def process_line(db_path: Path, line: str) -> None:
         return
 
     data = json.loads(line)
+    user_id = data["user_id"]
 
     # skip HTTP errors, invalid users, hidden users.
     if data["failed"]:
@@ -75,24 +77,50 @@ def process_line(db_path: Path, line: str) -> None:
     if data["data"]["response"]["snatched"] == "hidden":
         return
 
-    snatches = data["data"]["response"]["snatched"]
+    # grab the snatch data, skip compilation snatches (no artistId)
+    snatches = [i for i in data["data"]["response"]["snatched"] if "artistId" in i]
+    if not snatches:
+        return
 
-    group_tuples = [(i["groupId"], i["name"], i["artistName"]) for i in snatches]
-    snatch_tuples = [(data["user_id"], i["groupId"]) for i in snatches]
+    # get tuples needed for SQL inserts.
+    artist_tuples = set([(i["artistId"], i["artistName"]) for i in snatches])
+    group_tuples = set([(i["groupId"], i["artistId"], i["name"]) for i in snatches])
+    snatch_tuples = set([(user_id, i["groupId"]) for i in snatches])
+    user_tuple = (user_id, len(group_tuples))
+
     with sqlite3.connect(db_path) as conn:
         # always do this first, otherwise defaults to off
         conn.execute("pragma foreign_keys = on")
 
-        # insert groups first for foreign key support
-        # update on uniqueness violation
+        # insert artists first, they have no dependencies
         sql = """
-        insert into groups (group_id, name, artist_name) 
+        insert into artists (artist_id, name) 
+        values (?, ?)
+        on conflict(artist_id) do update set 
+            name=excluded.name
+        """
+        conn.executemany(sql, list(artist_tuples))
+
+        # insert users second, they also have no dependencies.
+        # The JSON should be unique on user, but in case it isnt just replace.
+        sql = """
+        insert into users (user_id, snatch_count) 
+        values (?, ?)
+        on conflict(user_id) do update set 
+            snatch_count=excluded.snatch_count
+        """
+        conn.execute(sql, user_tuple)
+
+        # insert groups third, they require artist and are needed for snatch
+        sql = """
+        insert into groups (group_id, artist_id, name) 
         values (?, ?, ?)
         on conflict(group_id) do update set
+            artist_id=excluded.artist_id,
             name=excluded.name,
-            artist_name=excluded.artist_name
+            snatch_count=snatch_count + 1
         """
-        conn.executemany(sql, group_tuples)
+        conn.executemany(sql, list(group_tuples))
 
         # add snatches, ignore duplicates.
         sql = """
@@ -148,6 +176,11 @@ if __name__ == "__main__":
         for line in tqdm.tqdm(f, total=total_lines):
             process_line(args.db_path, line)
 
-    # finish off with some additional indexes, views
+    # finish it off with some indexes
     with sqlite3.connect(args.db_path) as conn:
-        conn.execute("create index group_idx on snatches (group_id)")
+        sql = """
+        create index snatch_group_idx on snatches (group_id);
+        create index snatch_user_idx on snatches (user_id);
+        create index group_artist_idx on groups (artist_id);
+        """
+        conn.executescript(sql)
